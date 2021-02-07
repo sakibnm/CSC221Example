4b55d89cfa546c4f67e30f02ec94e0ae133bd3a9
Nazmus Sakib <sakibnm@gmail.com>
2021-02-07 18:30:15 -0500

    updated Account
    from mike

--- Week 1/Account/Account.java
+++ Week 1/Account/Account.java
@@ -2,6 +2,6 @@ package Account;
 
 class Account {
     public static void main(String[] args) {
-        System.out.println("Hello World Michael!");
+        System.out.println("Hello World Mike!");
     }
 }
